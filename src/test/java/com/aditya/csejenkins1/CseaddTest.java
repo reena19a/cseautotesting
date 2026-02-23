38a102de29d17491fca898a8af0ae8d60d927023
Reena <Reena@LAPTOP-PE02RK0S>
2026-02-23 14:48:58 +0530

    first commit

---
+++ src/test/java/com/aditya/csejenkins1/CseaddTest.java
@@ -0,0 +1,20 @@
+package com.aditya.csejenkins1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CseaddTest {
+	Cseadd c = new Cseadd();
+	@Test
+	void cseTest() {
+		int result = c.add(2, 3);
+		assertEquals(5,result);
+	}
+	@Test
+	    void itTest() {
+		int result = c.mul(2,3);
+		assertEquals(5,result);
+	}
+
+}
